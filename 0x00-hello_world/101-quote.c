af32f9ea2df042c801a3a1b9beea4bd905e615bf
David Michael <davidinmichael@gmail.com>
2022-12-08 18:56:36 +0100

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,7 @@
  * File: 101-quote.c
  * Auth: Brennan D Baraban
  */
-
 #include <unistd.h>
-
 /**
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  *                followed by a new line, to standard error.
