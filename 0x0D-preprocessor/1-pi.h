ba27b10bba36eb37a6bf51df299ef97b401e0f2f
davidinmicahel <davidinmichael@gmail.com>
2023-01-16 16:55:45 +0100

    I added the preprocessor files

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,6 @@
+#ifndef _1_PI_H_
+#define _1_PI_H_
+
+#define PI 3.14159265359
+
+#endif /* _1_PI_H_ */
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - prints the name ofthe file it was compiled form
+*
+* Return: zero
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
