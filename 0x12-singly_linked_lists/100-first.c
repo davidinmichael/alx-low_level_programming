50fd9db4447e5334b3840b4b52c2b7aad846b6e0
davidinmicahel <davidinmichael@gmail.com>
2023-01-27 10:57:01 +0100

    I modified the first.c file

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,16 @@
-#include <stdio.h>
+#include<stdio.h>
 
-void first(void) __attribute__ ((constructor));
 /**
- * first - prints something before main
- *
- * Return: void
+ * myStartupFun - Apply the constructor attribute to myStartupFun() so that it
+ *   is executed before main()
  */
-void first(void)
+void myStartupFun(void) __attribute__ ((constructor));
+
+/**
+ * myStartupFun - implementation of myStartupFun
+ */
+void myStartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
-}i /*Author: davidinmichael*/
+}
